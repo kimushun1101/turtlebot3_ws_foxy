a5b6545a8841bc0ccb5e9f6c6582567b8b444316
Shunsuke Kimura <kimushun1101@gmail.com>
2021-03-09 10:36:06 +0000

    MOD map scripts

--- scripts/navigation.sh
+++ scripts/navigation.sh
@@ -2,6 +2,9 @@
 source /opt/ros/foxy/setup.bash
 source /workspaces/turtlebot3_ws/install/setup.bash
 
+MAP_DIRECTORY="/workspaces/turtlebot3_ws/map"
+# MAP_DIRECTORY=$HOME
+
 # https://emanual.robotis.com/docs/en/platform/turtlebot3/nav_simulation/
 export TURTLEBOT3_MODEL=burger
-ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$HOME/map.yaml
+ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$MAP_DIRECTORY/map.yaml
--- scripts/save_map.sh
+++ scripts/save_map.sh
@@ -2,11 +2,5 @@
 source /opt/ros/foxy/setup.bash
 source /workspaces/turtlebot3_ws/install/setup.bash
 
-SAVE_DIRECTORY=$HOME
-# SAVE_DIRECTORY="/workspaces/turtlebot3_ws"
-
 # https://emanual.robotis.com/docs/en/platform/turtlebot3/slam_simulation/
-ros2 run nav2_map_server map_saver_cli -f $SAVE_DIRECTORY/map
-
-echo $SAVE_DIRECTORY && ls $SAVE_DIRECTORY
-read -p "input Enter"
+ros2 run nav2_map_server map_saver_cli -f $HOME/map
